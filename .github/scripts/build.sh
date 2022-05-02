9961b8c3b54f2222574ef2b801ee59aec6992bcf
Alejandro Mesa <notalemesa@gmail.com>
2022-05-02 18:43:34 -0400

    chore: gh actions update

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,12 +2,19 @@
 set -e
 
 #### Running build ####
-export COMMIT_ID=$(git rev-parse --short HEAD)
-export COMMIT_COUNT=$(git rev-list --no-merges --count HEAD)
+export COMMIT_ID=$(git log --pretty="%h" --no-merges -1)
+export COMMIT_DATE="$(git log --date=format:'%Y-%m-%d %H:%M' --pretty="%cd" --no-merges -1)"
+export COMMIT_TIME="$(git log --pretty="%at" --no-merges -1)"
+
+#### Print Environment Variables ####
+printenv
 
 rm -rf ./out
 
 npm run build:static
 
+if [ "$CI_ENV" != "production" ]; then
+  npm run build:storybook
+fi
 
 echo "$CI_COMMIT_ID/$CI_BUILD_ID" > out/VERSION.txt
