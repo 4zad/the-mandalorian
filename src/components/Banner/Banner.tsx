d69ab3a213450b4b6ea134fd78f07507b7597145
Vanessa Liu <liu_vanessa@rocketmail.com>
2022-05-11 15:11:04 -0400

    fix: updated banner component and resolved error

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -10,24 +10,17 @@ export type Props = {
 
 function Banner({ className }: Props) {
   return (
-    <>
-      <div className={classnames(styles.Banner, className)}>
-        {/* <h1>Banner component</h1> */}
-        {/* <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim vitae voluptates dicta ipsum eius quos maiores
-          libero cumque, asperiores quo laudantium, sit nam dolor iusto rerum provident saepe at placeat?
-        </p> */}
-        <div className={styles.imgwrapper}>
-          <img className={styles.bannerimg} src={bannerImage} alt="Mandalorian Billboard" />
-        </div>
-        <div className={styles.tags}>
-          <div className={styles.subtitle}>Tags</div>
-          <div className={styles.tagscontent}>
-            Trailer, Social Media Content, Disney, Disney+, Series, Drama, SCI FI, Featured
-          </div>
+    <div className={classnames(styles.Banner, className)}>
+      <div className={styles.imgWrapper}>
+        <img src={bannerImage} alt="Mandalorian Billboard" />
+      </div>
+      <div className={styles.tags}>
+        <div className={styles.subtitle}>Tags</div>
+        <div className={styles.tagsContent}>
+          Trailer. Social Media Content. Disney. Disney+. Series. Drama. SCI FI. Featured
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
