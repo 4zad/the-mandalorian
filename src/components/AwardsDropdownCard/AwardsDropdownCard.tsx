93f8dee9ec554e191c4d3292b573bc1a20869fb2
Vedaant Varshney <vedaant.varshney@gmail.com>
2022-05-11 13:15:26 -0400

    feature: added card design and content

--- src/components/AwardsDropdownCard/AwardsDropdownCard.tsx
+++ src/components/AwardsDropdownCard/AwardsDropdownCard.tsx
@@ -8,7 +8,23 @@ export type Props = {
 };
 
 function AwardsDropdownCard({ className }: Props) {
-  return <div className={classnames(styles.AwardsDropdownCard, className)}>AwardsDropdownCard component</div>;
+  return (
+    <div className={classnames(styles.AwardsDropdownCard, className)}>
+      <div className={styles.textHolder}>
+        <p className={styles.subtitle}>2019</p>
+        <p className={styles.awardBody}>Clio Awards: Theatrical Audio/Visual Technique</p>
+
+        <p className={styles.subtitle}>2020</p>
+        <p className={styles.awardBody}>Golden Trailers: Best Drama</p>
+
+        <p className={styles.subtitle}>2020</p>
+        <p className={styles.awardBody}>Promax: Program Trailer Promo</p>
+
+        <p className={styles.subtitle}>2020</p>
+        <p className={styles.awardBody}>Shortys: Twitter Video</p>
+      </div>
+    </div>
+  );
 }
 
 export default memo(AwardsDropdownCard);
