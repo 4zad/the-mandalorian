416fdefa4a4f3e940021673643b0c59647a1c8a1
Vedaant Varshney <vedaant.varshney@gmail.com>
2022-05-12 15:12:13 -0400

    feature: temp commit

--- src/components/FullScreenVideo/FullScreenVideo.tsx
+++ src/components/FullScreenVideo/FullScreenVideo.tsx
@@ -2,13 +2,14 @@ import { memo } from 'react';
 import classnames from 'classnames';
 
 import styles from './FullScreenVideo.module.scss';
+// import VideoGeneral from '@/components/VideoGeneral/VideoGeneral';
 
 export type Props = {
   className?: string;
 };
 
 function FullScreenVideo({ className }: Props) {
-  return <div className={classnames(styles.FullScreenVideo, className)}>FullScreenVideo component</div>;
+  return <div className={classnames(styles.FullScreenVideo, className)}></div>;
 }
 
 export default memo(FullScreenVideo);
