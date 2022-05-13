4c4831185e14c05b5e24580f99a65a5e0797495e
Vedaant Varshney <vedaant.varshney@gmail.com>
2022-05-13 16:23:28 -0400

    feature: video modal progress

--- src/components/FullScreenVideo/FullScreenVideo.tsx
+++ src/components/FullScreenVideo/FullScreenVideo.tsx
@@ -2,14 +2,22 @@ import { memo } from 'react';
 import classnames from 'classnames';
 
 import styles from './FullScreenVideo.module.scss';
-// import VideoGeneral from '@/components/VideoGeneral/VideoGeneral';
+import VideoGeneral from '@/components/VideoGeneral/VideoGeneral';
 
 export type Props = {
   className?: string;
 };
 
 function FullScreenVideo({ className }: Props) {
-  return <div className={classnames(styles.FullScreenVideo, className)}></div>;
+  return (
+    <div className={classnames(styles.FullScreenVideo, className)}>
+      {/* The size of the Image should match the dimensions of this div    */}
+      <div style={{ width: 500, height: 600 }}>
+        <VideoGeneral id={148751763} imlink={'assets/images/mandaloriantest.png'}></VideoGeneral>
+        {/* <VideoGeneral id={148751763} imlink={"mandaloriantest.png"}></VideoGeneral> */}
+      </div>
+    </div>
+  );
 }
 
 export default memo(FullScreenVideo);
