c665e2645cbe83f5d5ea45ab58acaa474ea84853
Vedaant Varshney <vedaant.varshney@gmail.com>
2022-05-12 20:29:20 -0400

    feature: vid impl broken

--- src/components/VideoGeneral/VideoGeneral.tsx
+++ src/components/VideoGeneral/VideoGeneral.tsx
@@ -1,20 +1,27 @@
 import { memo } from 'react';
 import classnames from 'classnames';
-import Vimeo from '@u-wave/react-vimeo';
+// import Vimeo from '@u-wave/react-vimeo';
+import Vimeo from '@vimeo/player';
 
 import styles from './VideoGeneral.module.scss';
 
 export type Props = {
   className?: string;
   isFullWidth?: boolean;
-  link: string;
+  divID: string;
+  id: number;
 };
 
 // Will take 100% of div width unless component has fullWidth = true, which will then be the full viewport width
-function VideoGeneral({ className, isFullWidth, link }: Props) {
+function VideoGeneral({ className, divID, id }: Props) {
+  const player = new Vimeo(divID, { id: id, width: 500 });
+  player.on('play', () => {
+    console.log('video was played');
+  });
+
   return (
     <div className={classnames(styles.VideoGeneral, className)}>
-      <Vimeo video={link} className={classnames({ [styles.isFullWidth]: isFullWidth })} />
+      <div id={divID} />
     </div>
   );
 }
