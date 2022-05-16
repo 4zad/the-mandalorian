99afb20e7eb7f1ef4bfecdad04befd81eaafc9a1
Vedaant Varshney <vedaant.varshney@gmail.com>
2022-05-16 13:21:29 -0400

    feature: video component generalization and prorgess

--- src/components/VideoGeneral/VideoGeneral.tsx
+++ src/components/VideoGeneral/VideoGeneral.tsx
@@ -1,45 +1,21 @@
-import { memo, useEffect, useRef, useState } from 'react';
+import { memo, useState } from 'react';
 import classnames from 'classnames';
-// import Vimeo from '@u-wave/react-vimeo';
-import Vimeo from '@vimeo/player';
-
-// import Image from '@/components/Image/Image';
-import PlayIcon from '@/components/svgs/play-icon.svg';
-import PlayCircle from '@/components/svgs/play-circle.svg';
 
 import styles from './VideoGeneral.module.scss';
 
+import PlayIcon from '@/components/svgs/play-icon.svg';
+import PlayCircle from '@/components/svgs/play-circle.svg';
+
+import VideoModal from '@/components/VideoModal/VideoModal';
+
 export type Props = {
   className?: string;
-  isFullWidth?: boolean;
-  id: number;
-  imlink: string;
+  imLink: string;
+  vidId: number;
 };
 
-// Will take 100% of div width unless component has fullWidth = true, which will then be the full viewport width
-function VideoGeneral({ className, id, imlink }: Props) {
-  const playerRef = useRef<HTMLDivElement>(null);
-
+function VideoGeneral({ className, imLink, vidId }: Props) {
   const [modalOpen, setModalOpen] = useState(false);
-  // css padding bottom trick
-  // css video modal close and open
-  useEffect(() => {
-    let options = {
-      id: id,
-      // pick width value somehow relative to the screen
-      width: 1000,
-      loop: false,
-      autoplay: false
-    };
-
-    if (playerRef.current !== null) {
-      let player = new Vimeo(playerRef.current, options);
-
-      player.on('play', () => {
-        console.log('play');
-      });
-    }
-  }, [id]);
 
   function buttonClick() {
     console.log('Button is clicked and modal is set to open');
@@ -52,8 +28,8 @@ function VideoGeneral({ className, id, imlink }: Props) {
         <PlayCircle className={styles.svgPlayCircle} />
         <PlayIcon className={styles.svgPlayIcon} />
       </button>
-      <img src={imlink}></img>
-      {modalOpen && <div className={styles.player} ref={playerRef} />}
+      <img src={imLink}></img>
+      {modalOpen && <VideoModal id={vidId} />}
     </div>
   );
 }
