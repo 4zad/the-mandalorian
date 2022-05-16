334dff6bf97f3cb94c5e64d8c950b1cdc5b4ebde
Vanessa Liu <liu_vanessa@rocketmail.com>
2022-05-16 14:10:19 -0400

    feature: started Process component

---
+++ src/components/Process/Process.stories.tsx
@@ -0,0 +1,9 @@
+import Process, { Props } from './Process';
+
+export default { title: 'components/Process' };
+
+export const Default = (args: Props) => <Process {...args} />;
+
+Default.args = {
+  // add control data
+};
