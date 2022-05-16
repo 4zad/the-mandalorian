f232f24ce25eb4ed628107a67d4a3a5dec0f32c4
Vedaant Varshney <vedaant.varshney@gmail.com>
2022-05-16 17:56:30 -0400

    feature: finished full screen video component

--- src/components/VideoGeneral/VideoGeneral.stories.tsx
+++ src/components/VideoGeneral/VideoGeneral.stories.tsx
@@ -5,8 +5,6 @@ export default { title: 'components/VideoGeneral' };
 export const Default = (args: Props) => <VideoGeneral {...args} />;
 
 Default.args = {
-  // add control data
-
   videoinf: {
     imLink: 'assets/images/mandaloriantest.png',
     vidId: 148751763
