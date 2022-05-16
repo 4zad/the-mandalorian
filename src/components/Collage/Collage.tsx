a9ffba140371a5c8679b8c96d82ebd617adda36c
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-05-16 19:51:51 -0400

    feature: 'Collage' component completed
    Implemented component using margins and transforms because CSS grids did not work as desired.
    
    As record, some CSS grids code has been left commented out in the respective stylesheet.
    
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- src/components/Collage/Collage.tsx
+++ src/components/Collage/Collage.tsx
@@ -17,7 +17,7 @@ export type Props = {
 function Collage({ className, images }: Props) {
   return (
     <div className={classnames(styles.collage, className)}>
-      <div className={classnames(styles.grid, styles.collageImages)}>
+      <div className={classnames(styles.collageImages)}>
         <Image
           src={images.image1}
           alt="Illustration of the Mandalorian at night"
@@ -30,7 +30,6 @@ function Collage({ className, images }: Props) {
           className={classnames(styles.image)}
         />
       </div>
-      {/* z-index: 1; // needed so the background text can be added behind later on */}
     </div>
   );
 }
