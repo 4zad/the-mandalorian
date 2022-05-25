981f5f36164b3d16e70e40e1c18864cd7ecee39f
Vanessa Liu <liu_vanessa@rocketmail.com>
2022-05-25 17:00:50 -0400

    feature: added text fade in for desc

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,5 +1,10 @@
-import { memo } from 'react';
+import { memo, useRef } from 'react';
 import classnames from 'classnames';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { textEase } from '@/data/eases';
+
+gsap.registerPlugin(ScrollTrigger);
 
 import styles from './Description.module.scss';
 
@@ -13,12 +18,47 @@ export type Props = {
   noPadding?: boolean;
 };
 
-function Description({ className, content, isSmallText = false, noPadding = false }: Props) {
+function Description({ className, content, isSmallText = false }: Props) {
+  let title = useRef<HTMLDivElement>(null);
+  let desc = useRef<HTMLDivElement>(null);
+
+  // useEffect(() => {
+  //   const tl = gsap.timeline();
+  //   tl.from(title.current, {
+  //     scrollTrigger: title.current,
+  //     duration: 1,
+  //     opacity: 0,
+  //     x: 40,
+  //     ease: textEase //'fadeIn'
+  //   });
+  //   //tl.current = gsap.timeline();
+
+  //   // gsap.from(title, {
+  //   //   duration: 1,
+  //   //   opacity: 0,
+  //   //   x: 40
+  //   // }
+  // }, []);
+
+  const tl = gsap.timeline();
+  tl.from(title.current, {
+    scrollTrigger: title.current,
+    duration: 1,
+    opacity: 0,
+    y: 40,
+    ease: textEase,
+    markers: true
+  }).from(desc.current, { scrollTrigger: desc.current, duration: 1, opacity: 0, y: 40, ease: textEase }, 0.96);
+
   return (
     <div className={classnames(styles.Description, className)}>
-      <div className={classnames(styles.textContainerPadding, { [styles.textContainer]: noPadding })}>
-        <p className={styles.title}>{content.title}</p>
-        <p className={classnames(styles.desc, { [styles.smallDesc]: isSmallText })}>{content.description}</p>
+      <div className={styles.textContainer}>
+        <p ref={title} className={styles.title}>
+          {content.title}
+        </p>
+        <p ref={desc} className={classnames(styles.desc, { [styles.smallDesc]: isSmallText })}>
+          {content.description}
+        </p>
       </div>
     </div>
   );
---
+++ src/data/eases.ts
@@ -0,0 +1,3 @@
+import gsap from 'gsap';
+
+export const textEase = gsap.parseEase('0.14, 1.00, 0.34, 1.00');
