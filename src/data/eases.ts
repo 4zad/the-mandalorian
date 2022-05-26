b5948b55fad77845410def9ac73ba35e6c15915e
Vedaant Varshney <vedaant.varshney@gmail.com>
2022-05-25 20:37:59 -0400

    feature: video general motion update

--- src/data/eases.ts
+++ src/data/eases.ts
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import gsap from 'gsap';
 
 export const mainEase = gsap.parseEase('0.14, 1.00, 0.34, 1.00');
-=======
-export const bezier1 = '0.14, 1.00, 0.34, 1.00';
->>>>>>> 1bba002 (feature: video general motion update)
-=======
-export const bezier1 = '0.14, 1.00, 0.34, 1.00';
->>>>>>> 1bba002 (feature: video general motion update)
