35143700829758702e07385ef1b10517cbdfc201
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-05-30 19:22:14 -0400

    refactor: code refactored and animations tweaked
    - ease bezier curves moved to data file
    
    - animations tweaked to resemble the animations on the live site more
    
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- src/data/eases.ts
+++ src/data/eases.ts
@@ -2,4 +2,6 @@ import { gsap } from 'gsap';
 
 export const mainEase = gsap.parseEase('0.14, 1.00, 0.34, 1.00');
 export const linearEase = gsap.parseEase('0.0, 0.0, 1.0, 1.0');
-export const temp = null;
+export const easeInOut: gsap.EaseFunction = gsap.parseEase(`0.9, 0.0, 0.1, 1.0`);
+export const easeInHero1: gsap.EaseFunction = gsap.parseEase(`1.0, 0.0, 0.7, 1.0`);
+export const easeInHero2: gsap.EaseFunction = gsap.parseEase(`0, 0.3, 0.0, 1.0`);
