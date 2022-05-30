95bb59d06fffa5f320bc87de8ad4195237ff3909
Vedaant Varshney <vedaant.varshney@gmail.com>
2022-05-30 17:55:20 -0400

    style: refactored animation and svg

--- src/components/VideoGeneral/VideoGeneral.tsx
+++ src/components/VideoGeneral/VideoGeneral.tsx
@@ -2,11 +2,6 @@ import { memo, useRef, useEffect } from 'react';
 import classnames from 'classnames';
 
 import styles from './VideoGeneral.module.scss';
-
-// Will not delete until confirmation of best practices
-// import PlayIcon from '@/components/svgs/play-icon.svg';
-// import PlayCircle from '@/components/svgs/play-circle.svg';
-
 import { setVideoId, useAppDispatch } from '@/redux';
 import gsap from 'gsap';
 import { mainEase } from '@/data/eases';
@@ -22,7 +17,6 @@ function VideoGeneral({ className, imLink, vidId, alt }: Props) {
   const dispatch = useAppDispatch();
   const vidGenRef = useRef<HTMLDivElement>(null);
   const playCircleRef = useRef<SVGSVGElement>(null);
-  const playIconRef = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
     gsap.from(vidGenRef.current, {
@@ -44,16 +38,6 @@ function VideoGeneral({ className, imLink, vidId, alt }: Props) {
       duration: 0.667,
       scrollTrigger: { trigger: vidGenRef.current, start: 'center bottom' }
     });
-
-    gsap.from(playIconRef.current, {
-      scaleX: 0.8,
-      scaleY: 0.8,
-      opacity: 0,
-      ease: mainEase,
-      duration: 0.667,
-      delay: 0.067,
-      scrollTrigger: { trigger: vidGenRef.current, start: 'center bottom' }
-    });
   }, []);
 
   return (
@@ -63,29 +47,22 @@ function VideoGeneral({ className, imLink, vidId, alt }: Props) {
           dispatch(setVideoId(vidId));
         }}
       >
-        {/* SVGs were included like this as refs weren't able to be passed on correctly */}
         <svg
-          className={styles.svgPlayCircle}
           ref={playCircleRef}
+          className={styles.svgPlayCircle}
           width="73"
           height="73"
           viewBox="0 0 73 73"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <circle cx="36.2082" cy="36.6994" r="35.1906" stroke="white" strokeWidth="1.5" />
-        </svg>
-        <svg
-          className={styles.svgPlayIcon}
-          ref={playIconRef}
-          width="12"
-          height="15"
-          viewBox="0 0 12 15"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
           <path
-            d="M0.556533 7.24113L0.55631 2.42834C0.556234 0.777227 2.44379 -0.162797 3.76148 0.832134L10.742 6.10286C11.8018 6.90306 11.8018 8.49489 10.742 9.29509L3.76146 14.5658C2.44378 15.5608 0.556223 14.6207 0.556292 12.9696L0.556533 7.24113Z"
+            d="M36.2082 71.89C55.6434 71.89 71.3988 56.1346 71.3988 36.6994C71.3988 17.2642 55.6434 1.5088 36.2082 1.5088C16.773 1.5088 1.01759 17.2642 1.01759 36.6994C1.01759 56.1346 16.773 71.89 36.2082 71.89Z"
+            stroke="white"
+            strokeWidth="1.5"
+          />
+          <path
+            d="M31.0002 35.8165L31 31.0037C30.9999 29.3526 32.8875 28.4126 34.2052 29.4075L41.1857 34.6783C42.2455 35.4785 42.2455 37.0703 41.1857 37.8705L34.2052 43.1412C32.8875 44.1362 30.9999 43.1961 31 41.545L31.0002 35.8165Z"
             fill="white"
           />
         </svg>
