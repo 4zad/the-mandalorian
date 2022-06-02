af04332b99d696b8ce0c5c84cd72e84488e10203
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-06-02 13:54:16 -0400

    feature: animation for hamburger menu is finished
    - on click of the toggle, changes from hamburger to X and then back again on click
    
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- src/data/eases.ts
+++ src/data/eases.ts
@@ -5,3 +5,4 @@ export const linearEase = gsap.parseEase('0.0, 0.0, 1.0, 1.0');
 export const easeInOut: gsap.EaseFunction = gsap.parseEase(`0.9, 0.0, 0.1, 1.0`);
 export const easeInHero1: gsap.EaseFunction = gsap.parseEase(`1.0, 0.0, 0.7, 1.0`);
 export const easeInHero2: gsap.EaseFunction = gsap.parseEase(`0, 0.3, 0.0, 1.0`);
+export const easeSnap: gsap.EaseFunction = gsap.parseEase(`1.0, 0.0, 1.0, -0.1`);
