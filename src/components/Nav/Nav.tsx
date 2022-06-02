990ab02749782db63a909799625b2c3b97061dba
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-06-02 13:54:16 -0400

    feature: animation for hamburger menu is finished
    - on click of the toggle, changes from hamburger to X and then back again on click
    
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,5 @@
-// import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 import Link from 'next/link';
 import classnames from 'classnames';
 
@@ -6,6 +7,7 @@ import styles from './Nav.module.scss';
 
 import Image from '@/components/Image/Image';
 // import Logo from '@/components/svgs/buddha-logo.svg';
+import { easeSnap } from '@/data/eases';
 
 import { navLinks } from '@/data/data';
 
@@ -18,7 +20,57 @@ import { navLinks } from '@/data/data';
 // }));
 
 function Nav() {
-  // const containerRef = useRef<HTMLElement>(null);
+  const menuHamburgerRef = useRef<HTMLDivElement>(null);
+  const topBarRef = useRef<HTMLSpanElement>(null);
+  const bottomBarRef = useRef<HTMLSpanElement>(null);
+  const menuHamburgerTl = useRef<gsap.core.Timeline>();
+
+  useEffect(() => {
+    const menuHamburgerToggleHeight: number = 40;
+    const menuHamburgerBarHeight: number = 2;
+    const transformedColor: string = '#000';
+
+    menuHamburgerTl.current = gsap
+      .timeline({
+        paused: true,
+        reversed: true,
+        defaults: {
+          delay: 0.15,
+          duration: 0.6,
+          ease: easeSnap
+        }
+      })
+      .to(
+        topBarRef.current,
+        {
+          // move down 12.5% height of the parent
+          // formula -> (12.5% height of the parent - height of the element divided by 2)
+          y: menuHamburgerToggleHeight * 0.125 - 1 - menuHamburgerBarHeight / 2,
+          rotation: -45, // 45 degrees clockwise
+          backgroundColor: transformedColor
+        },
+        'start'
+      )
+      .to(
+        bottomBarRef.current,
+        {
+          // move up 12.5% height of the parent
+          // formula -> (- 12.5% height of the parent - height of the element divided by 2)
+          y: -menuHamburgerToggleHeight * 0.125 - menuHamburgerBarHeight / 2,
+          rotation: 45, // 45 degrees counter clockwise
+          backgroundColor: transformedColor,
+          scaleX: 4 / 3,
+          xPercent: -12.5
+        },
+        'start'
+      );
+  });
+
+  const toggle = () => {
+    if (menuHamburgerTl.current) {
+      menuHamburgerTl.current.reversed() ? menuHamburgerTl.current.play() : menuHamburgerTl.current.reverse();
+    }
+  };
 
   return (
     <nav className={classnames(styles.nav)}>
@@ -34,14 +86,10 @@ function Nav() {
           </li>
         </ul>
 
-        <ul className={styles.links}>
-          <li>
-            <svg className={styles.menuHamburger} viewBox="0 0 100 30" width="40" height="40">
-              <rect className={styles.topBar} y="0" rx="4"></rect>
-              <rect className={styles.bottomBar} y="30" x="20" rx="4"></rect>
-            </svg>
-          </li>
-        </ul>
+        <div className={styles.menuHamburger} ref={menuHamburgerRef} onClick={toggle}>
+          <span className={styles.topBar} ref={topBarRef}></span>
+          <span className={styles.bottomBar} ref={bottomBarRef}></span>
+        </div>
       </div>
     </nav>
   );
--- src/data/eases.ts
+++ src/data/eases.ts
@@ -3,3 +3,4 @@ import { gsap } from 'gsap';
 export const easeInOut: gsap.EaseFunction = gsap.parseEase(`0.9, 0.0, 0.1, 1.0`);
 export const easeInHero1: gsap.EaseFunction = gsap.parseEase(`1.0, 0.0, 0.7, 1.0`);
 export const easeInHero2: gsap.EaseFunction = gsap.parseEase(`0, 0.3, 0.0, 1.0`);
+export const easeSnap: gsap.EaseFunction = gsap.parseEase(`1.0, 0.0, 1.0, -0.1`);
